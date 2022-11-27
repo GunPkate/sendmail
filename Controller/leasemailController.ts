6720ae3e982557e0685589a4584c78c5bec4644c
GunPKate <gun@builk.com>
2022-11-27 19:40:15 +0700

    fix calDate function

--- Controller/leasemailController.ts
+++ Controller/leasemailController.ts
@@ -9,7 +9,8 @@ import leasingEmail from "./LeasingEmail";
 import { sendmail_leasing } from "../function/sendmail_leasing";
 
 const api_path = "lease";
-const debtlist_query = `
+
+const allCustomer = `
 Select 
 c.customerId,	
 FirstName,	
@@ -19,6 +20,7 @@ balance,
 dueAt,
 dt.debtTypeID,
 dt.debtType,
+ds.debtstatusId,
 ds.debtstatus
 
 from customers c 
@@ -28,6 +30,20 @@ join debttypes dt on dt.DebtTypeId = db.debtTypeID
 join debtstatus ds on ds.debtstatusId = db.debtstatusId
 `;
 
+const debtlist_query =
+  allCustomer +
+  `
+ where ds.debtstatusId = '2'
+`;
+
+const calDate = async (item: any) => {
+  const now = await moment();
+  let date = await moment(item.dueAt, "YYYY-MM-DD");
+  let lateDue = await now.diff(date, "days");
+  console.log(now.format("YYYY-MM-DD"), date.format("YYYY-MM-DD"), lateDue);
+  return lateDue;
+};
+
 export const leaseMailController = (con: DataSource): Array<ServerRoute> => {
   const customer = con.getRepository(CustomerEntity);
   const debt = con.getRepository(DebtEntity);
@@ -35,9 +51,22 @@ export const leaseMailController = (con: DataSource): Array<ServerRoute> => {
   return [
     {
       method: "GET",
-      path: `/${api_path}`,
+      path: `/${api_path}/find`,
       handler: async (request: Request, h: ResponseToolkit, err?: Error) => {
         return debt.find();
+      },
+    },
+    {
+      method: "GET",
+      path: `/${api_path}/allCustomer`,
+      handler: async (request: Request, h: ResponseToolkit, err?: Error) => {
+        const detail = await con.query(allCustomer);
+        console.log(detail);
+
+        return h.response({
+          Data: detail,
+          Count: detail.length,
+        });
       },
     },
     {
@@ -60,16 +89,14 @@ export const leaseMailController = (con: DataSource): Array<ServerRoute> => {
         const debtList = await con.query(debtlist_query);
         let array = [];
         if (debtList) {
-          const now = moment();
-          array = debtList.filter((item: { dueAt: any }) => {
-            let date = moment(item.dueAt).format("YYYY-MM-DD");
-            let dateCal = moment(date.split("-"));
-            let lateDue = now.diff(dateCal, "days");
-            return lateDue > 0;
-          });
+          array = debtList.filter(
+            async (item: { dueAt: any; debtstatusId: number }) => {
+              return await calDate(item) > 0;
+            }
+          );
           for (let i = 0; i < array.length; i++) {
-            console.log(array[i]);
-            sendmail_leasing(array[i], array[i].email);
+            // console.log(array[i]);
+            // sendmail_leasing(array[i], array[i].email);
             // sendmail_leasing(debtList[i],debtList[i].email);
           }
         }
@@ -87,11 +114,8 @@ export const leaseMailController = (con: DataSource): Array<ServerRoute> => {
         const debtList = await con.query(debtlist_query);
         if (debtList) {
           const now = moment();
-          array = debtList.filter((item: { dueAt: any }) => {
-            let date = moment(item.dueAt).format("YYYY-MM-DD");
-            let dateCal = moment(date.split("-"));
-            let lateDue = now.diff(dateCal, "days");
-            return lateDue > 0;
+          array = debtList.filter(async (item: { dueAt: any }) => {
+            return await calDate(item) > 0;
           });
           for (let i = 0; i < array.length; i++) {
             console.log(array[i]);
--- x_sql.sql
+++ x_sql.sql
@@ -3,6 +3,7 @@ CREATE DATABASE leasing;
 
 insert into customerdetails (FirstName,LastName,email,contact,address) VALUES('สมชาย','สว่าง','gundash1@gmail.com','0822224455','Bangkok');
 insert into customerdetails (FirstName,LastName,email,contact,address) VALUES('ทมิฬ','มือไว','gundash1@hotmail.com','0822224655','Bangkok');
+insert into customerdetails (FirstName,LastName,email,contact,address) VALUES('ดำ','ร้อนเงิน','gpu.gun_st@tni.ac.th','0822224644','Bangkok');
 
 insert into debttypes (debttype) VALUES('สินเชื่อส่วนบุคคล');
 insert into debttypes (debttype) VALUES('หนี้บัตรเครดิต');
@@ -18,13 +19,19 @@ insert into employees (firstName,lastName,departmentId) VALUES('ละมุน'
 
 insert into customers (CustomerDetailId) VALUES(1);
 insert into customers (CustomerDetailId) VALUES(2);
+insert into customers (CustomerDetailId) VALUES(3);
 
 INSERT into debts (balance,customerId,debtTypeID,debtstatusid) values (7000,1,1,2);
 INSERT into debts (balance,customerId,debtTypeID,dueAt,debtstatusid) values (7000,2,1,'2022-06-21',1);
 INSERT into debts (balance,customerId,debtTypeID,dueAt,debtstatusid) values (20000,2,1,'2022-06-21',2);
 INSERT into debts (balance,customerId,debtTypeID,dueAt,debtstatusid) values (200000,2,1,'2022-09-21',2);
+INSERT into debts (balance,customerId,debtTypeID,dueAt,debtstatusid) values (7000,3,1,'2022-06-21',2);
 
-Select * 
+Select 
+c.customerId,	FirstName,	LastName,email,	balance,	dueAt,dt.debtTypeID,dt.debtType,ds.debtstatusId,ds.debtstatus
+
 from customers c 
 Join customerdetails d on d.customerDetailId = c.customerDetailId
-JOIN debts db on db.customerId = c.customerId 
+JOIN debts db on db.customerId = c.customerId
+join debttypes dt on dt.DebtTypeId = db.debtTypeID
+join debtstatus ds on ds.debtstatusId = db.debtstatusId
